2b7e065d215a85d89eecb4d44f9cafca81a1d840
Hira Mara <hiramara@users.noreply.github.com>
2026-05-10 11:41:15 +0800

    feat: initialize weclone package with version metadata

---
+++ weclone/__init__.py
@@ -0,0 +1,6 @@
+# WeClone - A tool for cloning your WeChat persona using LLMs
+# Fork of xming521/WeClone
+
+__version__ = "0.1.0"
+__author__ = "WeClone Contributors"
+__description__ = "Clone your digital persona from WeChat chat history using LLMs"
